545efafb35c4f101f8cfee46f07a4d1be35e63a6
divyansh <mah.divyansh@gmail.com>
2023-02-08 16:58:35 +0530

    initial commit

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,37 +1,32 @@
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
 const TodoApp = () => {
   const [todos, setTodos] = useState([]);
-  const [currentTodo, setCurrentTodo] = useState('');
+  const [currentTodo, setCurrentTodo] = useState("");
   const [editingIndex, setEditingIndex] = useState(null);
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     if (editingIndex === null) {
-      setTodos([...todos, { text: currentTodo, completed: false }]);
-      setCurrentTodo('');
+      setTodos([...todos, { text: currentTodo }]);
+      setCurrentTodo("");
     } else {
       const newTodos = [...todos];
       newTodos[editingIndex].text = currentTodo;
       setTodos(newTodos);
       setEditingIndex(null);
-      setCurrentTodo('');
+      setCurrentTodo("");
     }
   };
-
-  const handleTodoClick = index => {
+  const handleTodoClick = (index) => {
     const newTodos = [...todos];
-    newTodos[index].completed = !newTodos[index].completed;
     setTodos(newTodos);
   };
-
-  const handleDeleteClick = index => {
+  const handleDeleteClick = (index) => {
     const newTodos = [...todos];
     newTodos.splice(index, 1);
     setTodos(newTodos);
   };
-
-  const handleEditClick = index => {
+  const handleEditClick = (index) => {
     setEditingIndex(index);
     setCurrentTodo(todos[index].text);
   };
@@ -39,20 +34,22 @@ const TodoApp = () => {
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input type="text" value={currentTodo} onChange={e => setCurrentTodo(e.target.value)} />
-        <button type="submit">{editingIndex === null ? 'Add Todo' : 'Update Todo'}</button>
+        <input
+          type="text"
+          value={currentTodo}
+          onChange={(e) => setCurrentTodo(e.target.value)}
+        />
+        <button type="submit">
+          {editingIndex === null ? "Add Todo" : "Update Todo"}
+        </button>
       </form>
-      <ul>
-        {todos.map((todo, index) => (
-          <li key={index}>
-            <span onClick={() => handleTodoClick(index)} style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
-              {todo.text}
-            </span>
-            <button onClick={() => handleEditClick(index)}>Edit</button>
-            <button onClick={() => handleDeleteClick(index)}>Delete</button>
-          </li>
-        ))}
-      </ul>
+      {todos.map((todo, index) => (
+        <div key={index}>
+          <span onClick={() => handleTodoClick(index)}>{todo.text}</span>
+          <button onClick={() => handleEditClick(index)}>Edit</button>
+          <button onClick={() => handleDeleteClick(index)}>Delete</button>
+        </div>
+      ))}
     </div>
   );
 };

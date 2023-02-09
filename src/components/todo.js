57a78925c9ae4b79d13dd735ac0d9463f1f47386
divyansh <mah.divyansh@gmail.com>
2023-02-09 17:07:03 +0530

    Adding local storage and css

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,11 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
 const TodoApp = () => {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(JSON.parse(localStorage.getItem("todos")) || []);
   const [currentTodo, setCurrentTodo] = useState("");
   const [editingIndex, setEditingIndex] = useState(null);
 
+  useEffect(() => {
+    const todos = localStorage.getItem("todos");
+    if (todos) {
+      setTodos(JSON.parse(todos));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (!currentTodo) {
+      return alert('Please enter any todo');
+    }
+
     if (editingIndex === null) {
       setTodos([...todos, { text: currentTodo }]);
       setCurrentTodo("");
@@ -32,26 +48,36 @@ const TodoApp = () => {
   };
 
   return (
-    <div>
+    <div className="todo-app">
+        <h2>Hello Guys,Kuch likh lo</h2>
+
       <form onSubmit={handleSubmit}>
         <input
           type="text"
           value={currentTodo}
           onChange={(e) => setCurrentTodo(e.target.value)}
+          placeholder="Add Todo"
+          className="todo-input"
         />
-        <button type="submit">
+        <button type="submit" className="todo-button">
           {editingIndex === null ? "Add Todo" : "Update Todo"}
         </button>
       </form>
-      {todos.map((todo, index) => (
-        <div key={index}>
-          <span onClick={() => handleTodoClick(index)}>{todo.text}</span>
-          <button onClick={() => handleEditClick(index)}>Edit</button>
-          <button onClick={() => handleDeleteClick(index)}>Delete</button>
-        </div>
-      ))}
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <div key={index} className="todo-item">
+            <span onClick={() => handleTodoClick(index)} className="todo-text">{todo.text}</span>
+            <div className="todo-actions">
+              <button onClick={() => handleEditClick(index)} disabled={editingIndex === index} className="todo-edit">Edit</button>
+              <button onClick={() => handleDeleteClick(index)} disabled={editingIndex === index} className="todo-delete">Delete</button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
 
+
 export default TodoApp;
+
